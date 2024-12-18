77ec386dc5c6f63082464157cef608ec0364ded5
gauravshresthh <gauravshresthh@gmail.com>
2024-12-18 22:52:25 +0545

    feat: swagger doc updated

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes';
 import roleRoutes from './routes/roleRoutes';
 import { connectDB } from './config/database';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocs } from './config/swagger';
 
 const app = express();
 
@@ -10,6 +12,11 @@ app.use(bodyParser.json());
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/roles', roleRoutes);
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs, { explorer: true })
+  );
 
 connectDB();
 
---
+++ src/config/swagger.ts
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+export const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for college dashboard.',
+      contact: {
+        name: 'Gaurav Shrestha',
+        email: 'test@example.com',
+      },
+      license: {
+        name: 'MIT',
+        url: 'https://opensource.org/licenses/MIT',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'], // Pointing to TypeScript route files
+};
+
+export const swaggerDocs = swaggerJSDoc(swaggerOptions);
