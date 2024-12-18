7936500b5c87edf2e6c85788f151b3c7e1c9bccb
gauravshresthh <gauravshresthh@gmail.com>
2024-12-19 00:06:50 +0545

    feat: security best practises

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,45 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import bodyParser from 'body-parser';
+import path from 'path';
+
 import userRoutes from './routes/userRoutes';
 import roleRoutes from './routes/roleRoutes';
-import { connectDB } from './config/database';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerDocs } from './config/swagger';
 
 const app = express();
 
+app.use(compression());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 500,
+  message: `Too many requests from this IP , please try again in a hour`,
+});
+
+app.use('/api', limiter);
 app.use(bodyParser.json());
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello! Welcome to Vocational Placement Management API.');
+});
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/roles', roleRoutes);
 app.use(
@@ -17,7 +47,5 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocs, { explorer: true })
 );
-
-connectDB();
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
+import http from 'http';
 import app from './app';
 import { config } from './config/config';
+import { connectDB } from './config/database';
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting Down....');
+  console.log(err);
+  process.exit(1);
+});
+
+connectDB();
+
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+  console.log(`Server is listening on port ${config.port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting Down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
