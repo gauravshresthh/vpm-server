42b54c80a4e58277f948f0df1c52e16f76d0bef6
gauravshresthh <gauravshresthh@gmail.com>
2024-12-18 21:53:56 +0545

    chore: setup complete

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -4,6 +4,7 @@ export const authorize = (roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction): void => {
     const user = req.user as { role: string };
 
+    console.log(req.user)
     if (!user || !roles.includes(user.role)) {
       res.status(403).json({ message: 'Forbidden: You do not have access' });
       return;
